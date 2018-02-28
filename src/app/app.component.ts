f99d3fb5d1fcbd5fe3085b8368b4d46452811ad9
Yevgeniy Poluektov <evgeniy.v.poluektov@gmail.com>
2018-02-28 14:08:24 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
-import Stomp from 'stompjs';
-import SockJS from 'sockjs-client';
-import $ from 'jquery';
+import {Component} from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-root',
@@ -9,31 +8,28 @@ import $ from 'jquery';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private serverUrl = 'http://192.168.1.119:8888/socket'
-  private title = 'WebSockets chat';
-  private stompClient;
+  serverUrl = 'http://127.0.0.1:8080/ws';
+  matrixId = 2;
+  stompClient;
+  token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsidGVzdGp3dHJlc291cmNlaWQiXSwidXNlcl9maXJzdF9uYW1lIjoiQWRtaW4iLCJ1c2VyX2lkIjoxLCJ1c2VyX2xhc3RfbmFtZSI6IkFkbWluIiwidXNlcl9uYW1lIjoiYWRtaW4iLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXSwiZXhwIjoxNTE5ODU0OTY2LCJhdXRob3JpdGllcyI6WyJNQVRSSVhfM19XUklURSIsIkFETUlOIl0sImp0aSI6ImMwYTA2M2Q2LTQ0MzctNDlkNC1hMjY0LTRlM2I2YzhhN2M1ZSIsImNsaWVudF9pZCI6InRlc3Rqd3RjbGllbnRpZCJ9.9mib_w_X5PqlELRynz_2GUoisaozCc3GaQ_Mty6aAIY';
 
-  constructor(){
+  constructor() {
     this.initializeWebSocketConnection();
   }
 
-  initializeWebSocketConnection(){
-    let ws = new SockJS(this.serverUrl);
+  initializeWebSocketConnection() {
+    const ws = new SockJS(this.serverUrl);
     this.stompClient = Stomp.over(ws);
-    let that = this;
-    this.stompClient.connect({}, function(frame) {
-      that.stompClient.subscribe("/chat", (message) => {
-        if(message.body) {
-          $(".chat").append("<div class='message'>"+message.body+"</div>")
+    // TODO uncomment after develop
+    // this.stompClient.debug = null;
+    const that = this;
+    this.stompClient.connect({'X-Authorization': this.token}, function (frame) {
+      that.stompClient.subscribe('/matrix/flags/' + that.matrixId, (message) => {
+        if (message.body) {
           console.log(message.body);
+          // TODO save dynamic matrix document flags
         }
-      });
+      }, {'X-Authorization': that.token});
     });
   }
-
-  sendMessage(message){
-    this.stompClient.send("/app/send/message" , {}, message);
-    $('#input').val('');
-  }
-
 }
